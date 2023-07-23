641d0e086122a2b81b6f7aab330e90105fc3cb84
Yves G <contact@webnumerik.fr>
2023-07-23 14:04:52 +0200

    feat:

--- app.js
+++ app.js
@@ -1,35 +1,29 @@
 const express = require("express");
 const getEuromillionList = require("./euroMillionList");
-const cors = require('cors');
-const path = require('path');
-
-
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
-const allowedOrigins = [
-  'https://curious-palmier-d08c6b.netlify.app/'
-];
+const allowedOrigins = ["https://curious-palmier-d08c6b.netlify.app/"];
 
 const corsOptions = {
   origin: (origin, callback) => {
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-          callback(null, true)
-      } else {
-          callback(new Error('Not allowed by CORS'));
-      }
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
   },
-  optionsSuccessStatus: 200
-}
+  optionsSuccessStatus: 200,
+};
 app.use(cors(corsOptions));
 app.get("/", async (req, res) => {
   const getEuromillionLister = await getEuromillionList();
   res.status(200).json(getEuromillionLister);
 });
 
-
-app.get('/ScrapedDataSave.json', (req, res) => {
-  res.sendFile(path.join(__dirname, 'ScrapedDataSave.json'));
+app.get("/ScrapedDataSave.json", (req, res) => {
+  res.sendFile(path.join(__dirname, "ScrapedDataSave.json"));
 });
-
 
 module.exports = app;
