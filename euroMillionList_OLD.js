42679592f709b0667b49c6e83d6d16327793117f
Yves G. <contact@webnumerik.fr>
2025-01-24 23:18:01 +0100

    feat:

---
+++ euroMillionList_OLD.js
@@ -0,0 +1,75 @@
+//Scrapper of Euromillion stats page
+const fetch = require("isomorphic-fetch");
+const cheerio = require("cheerio");
+// const fs = require("fs");
+
+// start of the program
+const getEuromillionList = async () => {
+  // function to get the raw data
+  const getRawData = (URL) => {
+    return fetch(URL)
+      .then((response) => response.text())
+      .then((data) => {
+        return data;
+      });
+  };
+
+  // URL for data
+  const URL =
+    "https://www.fdj.fr/jeux-de-tirage/euromillions-my-million/statistiques";
+  const euromillionRawData = await getRawData(URL);
+
+  // parsing the data
+  const parsedEuromillionData = cheerio.load(euromillionRawData);
+
+  // extracting the table data
+  const scrapedDataNumbers = [];
+  const scrapedDataStars = [];
+
+  //parsedEuromillion Numbers Data
+  parsedEuromillionData(
+    "#page-chart > section.bk-palmares.bk-expand.bk-expand-0ee5d473-1ec6-4254-b5d0-f1477fedfe83.euromillion > div > div > div.tab-content_wrapper > div.tab-content.is-active > table > tbody > tr "
+  ).each((index, element) => {
+    const tds = parsedEuromillionData(element).find("td");
+    const numero = parsedEuromillionData(tds[0]).text();
+    const nombreDeSorties = parsedEuromillionData(tds[1]).text();
+    const pourcentageDeSorties = parsedEuromillionData(tds[2]).text();
+    const dateDeSorties = parsedEuromillionData(tds[3]).text();
+
+    const tableRow = {
+      numero,
+      nombreDeSorties,
+      pourcentageDeSorties,
+      dateDeSorties,
+    };
+
+    scrapedDataNumbers.push(tableRow);
+  });
+
+  //parsedEuromillion Stars Data
+  parsedEuromillionData(
+    "#page-chart > section.bk-palmares.bk-expand.bk-expand-0ee5d473-1ec6-4254-b5d0-f1477fedfe83.euromillion > div > div > div.tab-content_wrapper > div:nth-child(2) > table > tbody > tr"
+  ).each((_index, element) => {
+    const tdsStars = parsedEuromillionData(element).find("td");
+    const numeroStars = parsedEuromillionData(tdsStars[0]).text();
+    const nombreDeSortiesStars = parsedEuromillionData(tdsStars[1]).text();
+    const pourcentageDeSortiesStars = parsedEuromillionData(tdsStars[2]).text();
+    const dateDeSortiesStars = parsedEuromillionData(tdsStars[3]).text();
+
+    const tableRowStars = {
+      numeroStars,
+      nombreDeSortiesStars,
+      pourcentageDeSortiesStars,
+      dateDeSortiesStars,
+    };
+
+    scrapedDataStars.push(tableRowStars);
+  });
+  const allScrapedData = [scrapedDataNumbers, scrapedDataStars];
+
+  return allScrapedData;
+};
+
+// invoking the main function
+// getEuromillionList();
+module.exports = getEuromillionList;
